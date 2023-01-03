cae4a24ad934298b3330b41ed5d5a4798ed01734
Charlie19255 <charlie19255@gmail.com>
2023-01-03 13:54:57 -0800

    Added testing files for Header Footer Home and NotFound

---
+++ pig-tinder-frontend/src/Components/Footer.test.js
@@ -0,0 +1,6 @@
+import Footer from "./Footer"
+import { render, screen } from "@testing-library/react"
+describe("<Footer />", () => {
+    it("renders without crashing", () => {
+    })
+  })
---
+++ pig-tinder-frontend/src/Components/Header.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import userEvent from "@testing-library/user-event"
+import Header from "./Header"
+
+describe("<Header />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div")
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+      div
+    )
+  })
+  it("has clickable links", () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    )
+  })
+})
