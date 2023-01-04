e45420c872a19aa5d0a383608d763a023719044f
Charlie19255 <charlie19255@gmail.com>
2023-01-04 11:17:46 -0800

    Added indentations

--- pig-tinder-frontend/src/Pages/PigIndex.js
+++ pig-tinder-frontend/src/Pages/PigIndex.js
@@ -1,46 +1,35 @@
-import React from 'react'
-import {
-  Card, CardBody, CardSubtitle, CardTitle, Button
-}
-from "reactstrap"
-import { NavLink } from "react-router-dom"
-const PigIndex = ({pigs}) => {
-  console.log(pigs)
+import React from "react";
+import { Card, CardBody, CardSubtitle, CardTitle, Button } from "reactstrap";
+import { NavLink } from "react-router-dom";
+const PigIndex = ({ pigs }) => {
+  console.log(pigs);
   return (
-    
     <main className="pig-index-cards">
       <div>
-    {pigs?.map((pig, index) => {
-      return (
-        <Card
-        style={{
-          width: '18rem'
-        }}
-        key={index}
-      >
-       <img alt={`profile of a pig named ${pig.name}`} src={pig.image} />
-        <CardBody>
-          <CardTitle tag="h5">
-            {pig.name}
-          </CardTitle>
-          <CardSubtitle
-            className="mb-2 text-muted"
-            tag="h6"
-          >
-            {pig.breed}
-          </CardSubtitle>
-          <NavLink to={`/PigShow/${pig.id}`} className="nav-link">
-          <Button>
-            See more about me
-          </Button>
-          </NavLink>
-        </CardBody>
-      </Card>
-      )
-    })}
-  </div>
-  </main>
-  )
-}
+        {pigs?.map((pig, index) => {
+          return (
+            <Card
+              style={{
+                width: "18rem",
+              }}
+              key={index}
+            >
+              <img alt={`profile of a pig named ${pig.name}`} src={pig.image} />
+              <CardBody>
+                <CardTitle tag="h5">{pig.name}</CardTitle>
+                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                  {pig.breed}
+                </CardSubtitle>
+                <NavLink to={`/PigShow/${pig.id}`} className="nav-link">
+                  <Button>See more about me</Button>
+                </NavLink>
+              </CardBody>
+            </Card>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
 
-export default PigIndex
+export default PigIndex;
--- pig-tinder-frontend/src/Pages/PigIndex.test.js
+++ pig-tinder-frontend/src/Pages/PigIndex.test.js
@@ -1,18 +1,18 @@
-import { render, screen } from "@testing-library/react"
-import PigIndex from "./PigIndex"
-import mockPigs from "../mockPigs"
-import { BrowserRouter } from "react-router-dom"
+import { render, screen } from "@testing-library/react";
+import PigIndex from "./PigIndex";
+import mockPigs from "../mockPigs";
+import { BrowserRouter } from "react-router-dom";
 
 describe("<PigIndex />", () => {
   it("render a card for each pig", () => {
-   
-  render(
-    <BrowserRouter>
-  <PigIndex pigs={mockPigs} /> </BrowserRouter> )
-  mockPigs.map((pig) => {
-    const nameElement = screen.getByText(pig.name)
-    expect(nameElement).toBeInTheDocument()
-    
-  })
-})
-})
+    render(
+      <BrowserRouter>
+        <PigIndex pigs={mockPigs} />{" "}
+      </BrowserRouter>
+    );
+    mockPigs.map((pig) => {
+      const nameElement = screen.getByText(pig.name);
+      expect(nameElement).toBeInTheDocument();
+    });
+  });
+});
