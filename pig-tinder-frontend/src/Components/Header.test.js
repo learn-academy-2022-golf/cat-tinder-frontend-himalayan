d3d35aa15343507cf0d5bf2337c4f02a86124d6f
Charlie19255 <charlie19255@gmail.com>
2023-01-05 12:47:00 -0800

    Tests added back in

--- pig-tinder-frontend/src/Components/Header.test.js
+++ pig-tinder-frontend/src/Components/Header.test.js
@@ -1,17 +1,31 @@
-import { render, screen } from "@testing-library/react"
-import { BrowserRouter } from "react-router-dom"
-import userEvent from "@testing-library/user-event"
-import Header from "./Header"
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import Header from "./Header";
 
 describe("<Header />", () => {
   it("renders without crashing", () => {
-    const div = document.createElement("div")
+    const div = document.createElement("div");
     render(
       <BrowserRouter>
         <Header />
       </BrowserRouter>,
       div
-    )
-   
-  })
-})
+    );
+  });
+  it("has clickable links", () => {
+    const div = document.createElement("div");
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+      div
+    );
+    userEvent.click(screen.getByText("Show All Pigs"));
+    expect(screen.getByText("Show All Pigs")).toBeInTheDocument();
+    userEvent.click(screen.getByText("Home"));
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    userEvent.click(screen.getByText("Add to the mix"));
+    expect(screen.getByText("Add to the mix")).toBeInTheDocument();
+  });
+});
