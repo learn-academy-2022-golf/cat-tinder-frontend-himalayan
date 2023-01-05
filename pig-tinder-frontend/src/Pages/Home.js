cdac6fd62709e2db60183c99b93884459d2a2829
Kent Stone <69402437+kentstone84@users.noreply.github.com>
2023-01-05 12:20:13 -0500

    added the buttons and functionality back.
    Had to delete this work we did over christmas break because it jumped ahead of what we were doing with functionality.

--- pig-tinder-frontend/src/Pages/Home.js
+++ pig-tinder-frontend/src/Pages/Home.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
-  return (
-    <div>Welcome To the Home Page</div>
+  const [guineaPigs, setGuineaPigs] = useState([]) // Initialize empty array to store guinea pigs
 
+  const goToEdit = (index) => {
+    // Navigate to the edit page for the guinea pig at the given index
+  }
+
+  const seeIndex = () => {
+    // Navigate to the index page
+  }
+
+  const showExisting = () => {
+    // Fetch a list of existing guinea pigs from an API and display them
+  }
+
+  const addNew = () => {
+    // Navigate to the page for adding a new guinea pig
+  }
+
+  return (
+    <div>
+      <div style={{ fontSize: '20px', color: 'blue' }}>Welcome To the Home Page</div>
+      <button onClick={seeIndex} style={{ backgroundColor: 'green' }}>See Index</button>
+      <button onClick={showExisting} style={{ backgroundColor: 'orange' }}>Show Existing</button>
+      <button onClick={addNew} style={{ backgroundColor: 'purple' }}>Add New</button>
+      {guineaPigs.map((guineaPig, index) => (
+        <div key={guineaPig.id} style={{ border: '1px solid black' }}>
+          <div style={{ fontWeight: 'bold' }}>{guineaPig.name}</div>
+          <button onClick={() => goToEdit(index)} style={{ color
+     </div>
+      ))}
+     </div>
   )
 }
 
-export default Home 
+export default Home
