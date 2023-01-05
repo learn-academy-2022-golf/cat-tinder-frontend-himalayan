f9199a9ddadbdbead034a34f0666b8a17a6bfff4
Kent Stone <69402437+kentstone84@users.noreply.github.com>
2023-01-05 12:28:34 -0500

    Update Home.js

--- pig-tinder-frontend/src/Pages/Home.js
+++ pig-tinder-frontend/src/Pages/Home.js
@@ -4,27 +4,27 @@ const Home = () => {
   const [guineaPigs, setGuineaPigs] = useState([]) // Initialize empty array to store guinea pigs
 
   const goToEdit = (index) => {
-    // Navigate to the edit page for the guinea pig at the given index
+    //edit page for the guinea pig at the given index
   }
 
   const seeIndex = () => {
-    // Navigate to the index page
+    //index page
   }
 
   const showExisting = () => {
-    // Fetch a list of existing guinea pigs from an API and display them
+    //existing guinea pigs from an API and display them
   }
 
   const addNew = () => {
-    // Navigate to the page for adding a new guinea pig
+    //for adding a new guinea pig
   }
 
   return (
     <div>
-      <div style={{ fontSize: '20px', color: 'blue' }}>Welcome To the Home Page</div>
-      <button onClick={seeIndex} style={{ backgroundColor: 'green' }}>See Index</button>
-      <button onClick={showExisting} style={{ backgroundColor: 'orange' }}>Show Existing</button>
-      <button onClick={addNew} style={{ backgroundColor: 'purple' }}>Add New</button>
+      <div style={{ fontSize: '20px', color: 'blue' }}>Welcome To Guinea Pigs of the World</div>
+      <button onClick={seeIndex} style={{ backgroundColor: 'green' }}>See The Pigs</button>
+      <button onClick={showExisting} style={{ backgroundColor: 'orange' }}>Show All Pigs</button>
+      <button onClick={addNew} style={{ backgroundColor: 'purple' }}>Add to the mix</button>
       {guineaPigs.map((guineaPig, index) => (
         <div key={guineaPig.id} style={{ border: '1px solid black' }}>
           <div style={{ fontWeight: 'bold' }}>{guineaPig.name}</div>
